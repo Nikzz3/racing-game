cbe3a0264a4ecdf3fccf223fa53f5999b622776d
Nick Zimmermann <github.oo0su@slmails.com>
2026-09-13 13:22:43 +0200

    Allow all Kingfisher Pain Tailscale hosts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["nick-asus.kingfisher-pain.ts.net"],
+    allowedHosts: [".kingfisher-pain.ts.net"],
   },
 });
